937793920fcbd2b22dfbf77868cfee285a86c2a5
yxy-mz-k <1725532458@qq.com>
2026-09-08 17:46:49 +0800

    修改路径

--- src/store/modules/locale.ts
+++ src/store/modules/locale.ts
@@ -4,7 +4,7 @@ import { defineStore } from "pinia";
 import { store } from "store/index";
 
 import { cacheKeys } from "enums/cacheEnum";
-import { createLocalStorage } from "utils/cache";
+import { createLocalStorage } from "utils/cache/index";
 import { localeSetting } from "settings/localeSetting";
 
 const ls = createLocalStorage();
--- src/utils/cache/persistent.ts
+++ src/utils/cache/persistent.ts
@@ -2,7 +2,7 @@ import type { LockInfo, UserInfo } from "types/store";
 import type { ProjectConfig } from "types/config";
 import type { RouteLocationNormalized } from "vue-router";
 
-import { createLocalStorage, createSessionStorage } from "utils/cache";
+import { createLocalStorage, createSessionStorage } from "utils/cache/index";
 import { Memory } from "./memory";
 import { cacheKeys } from "enums/cacheEnum";
 import { DEFAULT_CACHE_TIME } from "settings/encryptionSetting";
